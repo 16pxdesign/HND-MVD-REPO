08473699b2d660955d49f9fd01ef313d580441c4
Aleksy <16pxdesign@gmail.com>
2019-02-24 06:28:25 +0000

    Add project files.

---
+++ HND GRADED UNIT SRU/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HND_GRADED_UNIT_SRU
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HND GRADED UNIT SRU/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HND_GRADED_UNIT_SRU.MvcApplication" Language="C#" %>
